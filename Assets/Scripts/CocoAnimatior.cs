2cef28769a2007d3a78ac47b3dca85a71834685b
linkabox <linkabox@foxmail.com>
2018-04-27 17:47:44 +0800

    Add CocoAnimatior

--- Assets/Scripts/CocoAnimatior.cs
+++ Assets/Scripts/CocoAnimatior.cs
@@ -1,31 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using LitJson;
+using UnityEditor;
 using UnityEngine;
 
 public class CocoAnimatior : MonoBehaviour
 {
+	public GameObject spritePrefab;
+	public string anim_name;
 	public int fps = 30;
 	public List<Sprite> sprites;
 	public float timer;
 
 	public int curFrame;
 	private float interval;
-	private SpriteRenderer renderer;
+	private List<SpriteRenderer> _spriteRenderers;
+	private CocoAnimMeta _animMeta;
 
 	// Use this for initialization
 	void Start()
 	{
-		renderer = this.GetComponent<SpriteRenderer>();
-		curFrame = 0;
-		if (sprites.Count > 0)
+		_animMeta = CocoResMgr.GetAnimMetaByName(anim_name);
+		if (_animMeta != null)
 		{
-			renderer.sprite = sprites[0];
+			sprites = new List<Sprite>();
+			foreach (int id in _animMeta.components)
+			{
+				sprites.Add(CocoResMgr.GetSprite(id));
+			}
+
+			_spriteRenderers = new List<SpriteRenderer>(_animMeta.max_render_count);
+			for (int i = 0; i < _animMeta.max_render_count; i++)
+			{
+				var go = Instantiate(spritePrefab, this.transform);
+				_spriteRenderers.Add(go.GetComponent<SpriteRenderer>());
+			}
 		}
+		curFrame = 0;
+		Play(curFrame);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		if (_animMeta == null) return;
 		if (sprites == null || sprites.Count == 0) return;
 
 		interval = 1f / fps;
@@ -37,18 +55,39 @@ public class CocoAnimatior : MonoBehaviour
 		}
 	}
 
+	void Play(int frame)
+	{
+		if (_animMeta == null || frame >= _animMeta.frames.Count) return;
+
+		var frameTrans = _animMeta.frames[frame];
+
+		for (int i = 0; i < frameTrans.Count; i++)
+		{
+			var ft = frameTrans[i];
+			var renderer = _spriteRenderers[i];
+			renderer.enabled = true;
+			renderer.sprite = sprites[ft.index];
+			E2DMatrix3x2.SetTransformFromMatrix(renderer.transform, ft.mat);
+		}
+
+		//禁用当前帧多余Renderer
+		for (int i = frameTrans.Count; i < _spriteRenderers.Count; i++)
+		{
+			_spriteRenderers[i].enabled = false;
+		}
+	}
+
 	void NextFrame()
 	{
 		int nextFrame = curFrame + 1;
-		if (nextFrame < sprites.Count)
+		if (nextFrame < _animMeta.frames.Count)
 		{
-			renderer.sprite = sprites[nextFrame];
 			curFrame = nextFrame;
+			Play(curFrame);
 		}
 		else
 		{
 			curFrame = 0;
-			renderer.sprite = sprites[curFrame];
 		}
 	}
 }
--- Assets/Scripts/CocoPackage.cs
+++ Assets/Scripts/CocoPackage.cs
@@ -4,38 +4,17 @@ using UnityEngine;
 
 public class CocoSpritaMeta
 {
-    public int id;
-    public string name;
-    public int texId;
-    public Rect rect;
+	public int id;
+	public string name;
+	public int texId;
+	public Rect rect;
 }
-
-public class CocoAnimMeta
-{
-    public int id;
-    public string name;
-    public List<int> refIds;
-
-    //ejoy2d每帧可能包含多个子Sprite对象的变换
-    public List<List<CocoAnimFrame>> frames;
-}
-
-public class CocoAnimFrame
-{
-    public int index;
-    public float[] mat; //matrix3x2
-}
-
-//public struct CocoMatrix3x2
-//{
-
-//}
 
 public class CocoPackage
 {
-    public string name;
-    public int texCount;
+	public string name;
+	public int texCount;
 
-    public List<CocoSpritaMeta> sprites;
-    public List<CocoAnimMeta> animations;
+	public List<CocoSpritaMeta> sprites;
+	public List<CocoAnimMeta> animations;
 }
